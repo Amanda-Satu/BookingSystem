d6598e9f0cd1eb03026f9ccfa3c6db1857feeacf
sphes <sphesihleslingile@gmail.com>
2025-03-27 11:29:27 +0200

    Sphesihle Slingile

---
+++ src/main/java/za/ac/cput/domain/Factory/ServiceFactory.java
@@ -0,0 +1,32 @@
+package za.ac.cput.domain.Factory;
+
+import za.ac.cput.domain.Service;
+/* author:sphesihle slingile
+230310257
+date: 26 March 2025
+ */
+public class ServiceFactory {
+    public static Service serviceCreation(int serviceId, String serviceName, double price,
+                                          String description){
+        if(serviceId <=0){
+            throw new IllegalArgumentException("serviceId must be greater than 0");
+        }
+        if(serviceName.isEmpty()){
+            throw new IllegalArgumentException("serviceName cannot be left empty");
+        }
+        if(price<=0){
+            throw new IllegalArgumentException("price must be greater than 0");
+        }
+        if(description.isEmpty()){
+            throw new IllegalArgumentException("description cannot be left empty");
+        }
+        return new Service.Builder()
+                .setServiceId(serviceId)
+                .setServiceName(serviceName)
+                .setPrice(price)
+                .setDescription(description)
+                .build();
+
+    }
+}
+
---
+++ src/test/java/za/ac/cput/domain/Factory/ServiceFactoryTest.java
@@ -0,0 +1,73 @@
+package za.ac.cput.domain.Factory;
+
+import static org.junit.jupiter.api.Assertions.*;
+/* author:sphesihle slingile
+230310257
+date: 26 March 2025
+ */
+import za.ac.cput.domain.Service;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServiceFactoryTest {
+    @Test
+    public void testServiceCreation(){
+        int serviceId = 1001;
+        String serviceName = "painting";
+        double price = 100.50;
+        String description = "painting the interior of house";
+
+        Service service1 = ServiceFactory.serviceCreation(serviceId, serviceName, price, description);
+
+        assertNotNull(service1);
+        assertEquals(serviceId, service1.getServiceId());
+        assertEquals(serviceName, service1.getServiceName());
+        assertEquals(price, service1.getPrice());
+        assertEquals(description, service1.getDescription());
+
+    }
+    @Test
+    public void testCreateServiceWithInvalidServiceId(){
+        int serviceId = -1001;
+        String serviceName = "painting";
+        double price = 100.50;
+        String description = "painting the interior of house";
+        IllegalArgumentException iaException = assertThrows(IllegalArgumentException.class, ()->{
+            ServiceFactory.serviceCreation(serviceId, serviceName, price, description);
+        });
+        assertEquals("serviceId must be greater than 0", iaException.getMessage());
+    }
+    @Test
+    public void testCreateServiceWithInvalidServiceName(){
+        int serviceId = 1001;
+        String serviceName = "";
+        double price = 100.50;
+        String description = "painting the interior of house";
+        IllegalArgumentException iaException = assertThrows(IllegalArgumentException.class, ()->{
+            ServiceFactory.serviceCreation(serviceId, serviceName, price, description);
+        });
+        assertEquals("serviceName cannot be left empty", iaException.getMessage());
+    }
+    @Test
+    public void testCreateServiceWithInvalidPrice(){
+        int serviceId = 1001;
+        String serviceName = "painting";
+        double price = -100.50;
+        String description = "painting the interior of house";
+        IllegalArgumentException iaException = assertThrows(IllegalArgumentException.class, ()->{
+            ServiceFactory.serviceCreation(serviceId, serviceName, price, description);
+        });
+        assertEquals("price must be greater than 0", iaException.getMessage());
+    }
+    @Test
+    public void testCreateServiceWithInvalidDescription(){
+        int serviceId = 1001;
+        String serviceName = "painting";
+        double price = 100.50;
+        String description = "";
+        IllegalArgumentException iaException = assertThrows(IllegalArgumentException.class, ()->{
+            ServiceFactory.serviceCreation(serviceId, serviceName, price, description);
+        });
+        assertEquals("description cannot be left empty", iaException.getMessage());
+    }}
+
